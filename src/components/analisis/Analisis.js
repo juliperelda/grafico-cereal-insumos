10f39d766d704fb3123cb617d0b4a12571425866
juliperelda <juliperelda@gmail.com>
2023-02-16 09:40:12 -0300

    Realizando pruebas

--- src/components/analisis/Analisis.js
+++ src/components/analisis/Analisis.js
@@ -7,7 +7,7 @@ export const Analisis = () => {
     return (
         <>
             <div className="divContainer">
-                <Card className="cardGraficoCerealEntregado" style={{ width: "50%", height: "700px" }}>
+                <Card className="cardGraficoCerealEntregado" style={{ width: "100%", height: "200px" }}>
                     <h1 className="titulos">ANALISIS CEREAL ENTREGADO</h1>
                     <GraficoCerealEntregado />
                 </Card>
